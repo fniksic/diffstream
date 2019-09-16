74220fb845a78659c3c413881734a8066efc8b92
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2019-09-16 18:58:14 -0400

    Successfuly use the Integer generator of junit-quickcheck

--- src/main/java/edu/upenn/streamstesting/InputGenerator.java
+++ src/main/java/edu/upenn/streamstesting/InputGenerator.java
@@ -2,6 +2,17 @@ package edu.upenn.streamstesting;
 
 import org.apache.flink.streaming.api.functions.source.SourceFunction;
 
+// Input generation
+import com.pholser.junit.quickcheck.generator.java.lang.IntegerGenerator;
+import com.pholser.junit.quickcheck.random.SourceOfRandomness;
+import com.pholser.junit.quickcheck.internal.generator.SimpleGenerationStatus;
+import com.pholser.junit.quickcheck.internal.GeometricDistribution;
+import java.util.Random;
+// import org.quicktheories.generators.IntegersDSL;
+// import org.quicktheories.core.Gen;
+// import org.quicktheories.core.RandomnessSource;
+// import org.quicktheories.core.XOrShiftPRNG;
+
 // KK: We should probably extend this to be a checkpointable source
 //
 // KK: This might have to be parametrized by some type??
@@ -17,16 +28,28 @@ public class InputGenerator implements SourceFunction<Integer> {
 	@Override
 	public void run(SourceFunction.SourceContext<Integer> sourceContext) {
 
-		// TODO:
-		//
-		// - Instantiate an integer generator from the junit-quickcheck library
-		//
-		// - Add junit-quickcheck as a Maven repo? As it seems
-		//   that we might not need to play with source?
-		//
-		// - Call the generate method of the integer generator with a sourceofRandomness
+		// TODO: In order to parametrize this, I could follow the tree from JUnitQuickcheck.java
+		
+		// Instantiate a new integer generator
+		// TODO: Make this all parametrizable
+		IntegerGenerator gen = new IntegerGenerator();
+
+		// Instantiate source of randomness and generation status to call the generator
+		Random randGen = new Random();
+		SourceOfRandomness rand = new SourceOfRandomness(randGen);
+		GeometricDistribution geo = new GeometricDistribution();
+	        SimpleGenerationStatus status = new SimpleGenerationStatus(geo, rand, 0);
+
+		// Quicktheories
+		// IntegersDSL intGen = new IntegersDSL();
+		// Gen<Integer> gen = intGen.all();
+		// // TODO: Have a random see maybe?
+		// XOrShiftPRNG prng = new XOrShiftPRGN(1);
+
 		while (isRunning) {
-			sourceContext.collect(1);
+			// Integer curr = gen.generate(prng);
+			Integer curr = gen.generate(rand, status);
+			sourceContext.collect(curr);
 		}
 	}
  
