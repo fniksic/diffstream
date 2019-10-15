779f42d0bee4c6c2266af15a96118c6734937a65
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2019-10-15 13:13:40 -0400

    Add a small flink pipeline and using jedis

--- src/test/java/edu/upenn/streamstesting/examples/wordcount/WordCountTest.java
+++ src/test/java/edu/upenn/streamstesting/examples/wordcount/WordCountTest.java
@@ -1,7 +1,11 @@
 package edu.upenn.streamstesting.examples.wordcount;
 
 import edu.upenn.streamstesting.examples.flinktraining.WindowsTest;
+import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.runtime.testutils.MiniClusterResourceConfiguration;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.test.util.MiniClusterWithClientResource;
 import org.junit.ClassRule;
 import org.junit.Test;
@@ -27,5 +31,36 @@ public class WordCountTest {
         String value = jedis.get("foo");
         System.out.println("Value for foo: " + value);
         assert(value.equals("bar"));
+    }
+
+    @Test
+    public void redisTest2 () {
+        Jedis jedis = new Jedis("localhost");
+        WordCount wc = new WordCount(jedis);
+        wc.fillJedisManual();
+        String value = jedis.get("haskell");
+        assert(value.equals("science"));
+        String value2 = jedis.get("java");
+        assert(value2.equals("bean"));
+    }
+
+    @Test
+    public void manualWordCountTest() throws Exception {
+
+        // Initialize and fill Jedis
+        WordCount wc = new WordCount();
+        wc.fillJedisManual();
+
+        // Initialize the FLink environment
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+
+        DataStreamSource<String> source = env.fromElements("haskell", "science");
+
+        DataStream<Tuple2<String, String>> output = wc.sequentialComputation(source);
+
+        output.print();
+
+        env.execute();
     }
 }
