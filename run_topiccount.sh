41815f9e2e5aaf05467f0bbccdf7b6563e949d6e
Filip Niksic <fniksic@seas.upenn.edu>
2020-08-09 00:11:38 -0400

    Change run_topiccount.sh to run all the commands from the corresponding evaluation section

--- run_topiccount.sh
+++ run_topiccount.sh
@@ -2,6 +2,10 @@
 
 cd examples/topiccount
 ./prepare-topiccount.sh
-./run-topiccount.sh par-scaleup --totalDocuments 2 --wordsPerDocument 10000
+./run-topiccount.sh sequential
+./run-topiccount.sh parallel
+./run-topiccount.sh seq-diff-test --totalDocuments 1 --wordsPerDocument 1
+./run-topiccount.sh par-diff-test
+./run-topiccount.sh par-scaleup
 ./teardown-topiccount.sh
 cd ../..
