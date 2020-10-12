c516e2ee6b22aee57de3e7930ab21f224ee06f62
Filip Niksic <fniksic@seas.upenn.edu>
2020-10-12 00:58:21 -0400

    Update the plotting script

--- data/online-monitoring/collect_and_plot.py
+++ data/online-monitoring/collect_and_plot.py
@@ -21,27 +21,46 @@ def parse_memory_line(line):
     return int(memory_string)
 
 
-def plot_memories_in_time(dir_name, mems_one, mems_two):
+def plot_memory_in_time(dir_name, mems_one, mems_two):
     size_one = len(mems_one)
     size_two = len(mems_two)
 
-    width = 0.5
-
-    x_one = np.linspace(0, size_one, size_one) - (width / 2)
-    x_two = np.linspace(0, size_two, size_two) + (width / 2)
+    x_one = np.linspace(0, size_one, size_one)
+    x_two = np.linspace(0, size_two, size_two)
 
     fig, ax = plt.subplots()
-    ax.bar(x_one, mems_one, width, label='With matcher')
-    ax.bar(x_two, mems_two, width, label='Without matcher')
+    ax.scatter(x_one, mems_one, 0.2, label='With matcher')
+    ax.scatter(x_two, mems_two, 0.2, label='Without matcher', alpha=0.7)
     ax.set_xticks([0, 1800, 3600, 5400, 7200])
     ax.set_xlabel("Time (seconds)")
     ax.set_ylabel("Used memory (MB)")
-    ax.legend()
+    ax.legend(markerscale=10)
 
-    plt.yscale('log')
     plt.tight_layout()
     # plt.show()
     plt.savefig(path.join(dir_name, "used_memory_in_time.pdf"))
+
+
+def plot_memory_ccdfs(dir_name, mems_one, mems_two):
+    unique_mems_one, counts_one = np.unique(mems_one, return_counts=True)
+    np.cumsum(counts_one, out=counts_one)
+    fraction_one = 1.0 - counts_one / float(len(mems_one))
+
+    unique_mems_two, counts_two = np.unique(mems_two, return_counts=True)
+    np.cumsum(counts_two, out=counts_two)
+    fraction_two = 1.0 - counts_two / float(len(mems_two))
+
+    fig, ax = plt.subplots()
+    ax.plot(unique_mems_one, fraction_one, label='With matcher')
+    ax.plot(unique_mems_two, fraction_two, label='Without matcher', alpha=0.7)
+    ax.set_xlabel('Used memory (MB)')
+    ax.set_ylabel('Complementary cumulative fraction')
+    ax.legend()
+    plt.yscale('log')
+
+    plt.tight_layout()
+    # plt.show()
+    plt.savefig(path.join(dir_name, 'memory_ccdf.pdf'))
 
 
 def parse_unmatched(dir_name):
@@ -61,15 +80,18 @@ def parse_unmatched_line(line):
 
 
 def process_unmatched(unmatched):
+    cut_point = next(i for i in reversed(range(len(unmatched))) if unmatched[i][3] != unmatched[len(unmatched) - 1][3])
+    unmatched = unmatched[0:cut_point]
     timestamps = [(tup[0] - unmatched[0][0]).total_seconds() for tup in unmatched]
-    left_unmatched = [tup[1] for tup in unmatched]
-    right_unmatched = [tup[2] for tup in unmatched]
     total_unmatched = [tup[1] + tup[2] for tup in unmatched]
-    avg_window = 1
-    throughput = [(unmatched[i][3] - unmatched[max(0, i - avg_window)][3]) /
+    throughput = [(unmatched[i][3] - unmatched[i - 1][3]) /
+                  (timestamps[i] - timestamps[i - 1])
+                  for i in range(1, len(unmatched))]
+    avg_window = 60
+    throughput_avg = [(unmatched[i][3] - unmatched[max(0, i - avg_window)][3]) /
                   (timestamps[i] - timestamps[max(0, i - avg_window)])
                   for i in range(1, len(unmatched))]
-    return timestamps, left_unmatched, right_unmatched, total_unmatched, throughput
+    return timestamps, total_unmatched, throughput, throughput_avg
 
 
 def get_latencies(dir_name):
@@ -77,16 +99,12 @@ def get_latencies(dir_name):
     return latencies
 
 
-def plot_unmatched_in_time(dir_name, timestamps, left_unmatched, right_unmatched, total_unmatched):
+def plot_unmatched_in_time(dir_name, timestamps, total_unmatched):
     fig, ax = plt.subplots()
-    #width = 0.3
-    #ax.bar([ts - width for ts in timestamps], left_unmatched, width, label='Left')
-    ax.bar(timestamps, total_unmatched, label='Total unmatched')
-    #ax.bar([ts + width for ts in timestamps], right_unmatched, width, label='Right')
+    ax.scatter(timestamps, total_unmatched, 0.2, label='Total unmatched')
     ax.set_xticks([0, 1800, 3600, 5400, 7200])
     ax.set_xlabel("Time (seconds)")
     ax.set_ylabel("Number of unmatched events")
-    # ax.legend()
 
     plt.tight_layout()
     # plt.show()
@@ -98,43 +116,57 @@ def plot_unmatched_histogram(dir_name, total_unmatched):
     ax.hist(total_unmatched, bins=100)
     ax.set_ylabel("Number of samples (taken every 1 second)")
     ax.set_xlabel("Number of unmatched events")
+    plt.show()
+    # plt.savefig(path.join(dir_name, "unmatched_histogram.pdf"))
+
+
+def plot_unmatched_ccdf(dir_name, total_unmatched):
+    unique_unmatched, counts = np.unique(total_unmatched, return_counts=True)
+    np.cumsum(counts, out=counts)
+    fractions = 1.0 - counts / float(len(total_unmatched))
+
+    fig, ax = plt.subplots()
+    ax.plot(unique_unmatched, fractions)
+    ax.set_xlabel('Number of unmatched events')
+    ax.set_ylabel('Complementary cumulative fraction')
+    plt.yscale('log')
 
     plt.tight_layout()
     # plt.show()
-    plt.savefig(path.join(dir_name, "unmatched_histogram.pdf"))
+    plt.savefig(path.join(dir_name, 'unmatched_ccdf.pdf'))
 
 
-def plot_throughput(dir_name, ts_one, throughput_one, ts_two, throughput_two):
+def plot_throughput(dir_name, ts_one, throughput_one, throughput_avg_one, ts_two, throughput_two, throughput_avg_two):
     fig, ax = plt.subplots()
-    ax.plot(ts_one, throughput_one, label='With matcher', linewidth=0.5)
-    ax.plot(ts_two, throughput_two, label='Without matcher', linewidth=0.5)
-    ax.set_xticks([0, 1800, 3600, 5400, 7200])
-    ax.set_ylabel('Throughput (events per second)')
+    # ax.plot(ts_one, throughput_one, label='With matcher', linewidth=0.2)
+    # ax.plot(ts_two, throughput_two, label='Without matcher', linewidth=0.2)
+    ax.plot(ts_one, throughput_avg_one, label='With matcher', linewidth=0.2)
+    ax.plot(ts_two, throughput_avg_two, label='Without matcher', linewidth=0.2, alpha=0.7)
+    # ax.vlines(470, 0, 42000, linewidth=0.5, colors='gray')
+    # ax.set_xticks([0, 470, 1800, 3600, 5400, 7200])
+    ax.vlines([1737, 2023], 0, 42000, linewidth=0.5, colors='gray')
+    ax.set_xticks([0, 625, 1250, 1737, 2023, 2500])
+    ax.set_ylabel('Throughput (events/s)')
     ax.set_xlabel('Time (seconds)')
     ax.legend()
 
     plt.tight_layout()
-    plt.show()
-    # plt.savefig(path.join(dir_name, 'throughput.pdf'))
+    # plt.show()
+    plt.savefig(path.join(dir_name, 'throughput-accelerated.pdf'))
 
 
-def plot_latencies(dir_name, latencies_one, latencies_two):
-    unique_lats_one, counts_one = np.unique(latencies_one, return_counts=True)
-    np.cumsum(counts_one, out=counts_one)
-    fractions_one = counts_one / float(len(latencies_one))
-
-    unique_lats_two, counts_two = np.unique(latencies_two, return_counts=True)
-    np.cumsum(counts_two, out=counts_two)
-    fractions_two = counts_two / float(len(latencies_two))
+def plot_latencies(dir_name, latencies):
+    unique_lats, counts = np.unique(latencies, return_counts=True)
+    np.cumsum(counts, out=counts)
+    fractions = 1.0 - counts / float(len(latencies))
 
     fix, ax = plt.subplots()
-    ax.plot(unique_lats_one, fractions_one, label='With matcher')
-    ax.plot(unique_lats_two, fractions_two, label='Without matcher')
+    ax.plot(unique_lats, fractions)
     ax.set_xlabel('Latency (microseconds)')
-    ax.set_ylabel('Cumulative fraction')
-    ax.legend()
-
+    ax.set_ylabel('Complementary cumulative fraction')
     plt.xscale('log')
+    plt.yscale('log')
+
     plt.tight_layout()
     # plt.show()
     plt.savefig(path.join(dir_name, 'latencies.pdf'))
@@ -171,33 +203,39 @@ def main():
 
     plt.rcParams['mathtext.fontset'] = 'stix'
     plt.rcParams['font.family'] = 'STIXGeneral'
+    plt.rcParams['pdf.fonttype'] = 42
 
-    dir_one = "server_load_6000_time_300_test"
-    dir_two = "server_load_6000_time_300_test"
     out_dir = "."
 
-    # if(not len(sys.argv) == 2):
-    #     print("Wrong arguments")
-    #     print("You should call this as: `python3 collect_and_plot.py <results_dir>`")
-    #     exit(1)
-    #
-    # dir_name = sys.argv[1]
+    # Plot accelerated throughput
 
-    # mems_one = parse_memories(dir_one)
-    # mems_two = parse_memories(dir_two)
-    # plot_memories_in_time(out_dir, mems_one, mems_two)
+    dir_one = "server_load_40000_accel_10_time_2500_real_throughput"
+    dir_two = "server_load_40000_accel_10_time_2500_dummy_throughput"
 
     items_one = parse_unmatched(dir_one)
-    ts_one, left_unmatched_one, right_unmatched_one, total_unmatched_one, throughput_one = process_unmatched(items_one)
+    ts_one, total_unmatched_one, throughput_one, throughput_avg_one = process_unmatched(items_one)
     items_two = parse_unmatched(dir_two)
-    ts_two, left_unmatched_two, right_unmatched_two, total_unmatched_two, throughput_two = process_unmatched(items_two)
-    # plot_unmatched_in_time(out_dir, ts_one, left_unmatched_one, right_unmatched_one, total_unmatched_one)
-    # plot_unmatched_histogram(out_dir, total_unmatched_one)
-    plot_throughput(out_dir, ts_one[1:], throughput_one, ts_two[1:], throughput_two)
+    ts_two, total_unmatched_two, throughput_two, throughput_avg_two = process_unmatched(items_two)
+    plot_throughput(out_dir, ts_one[1:], throughput_one, throughput_avg_one, ts_two[1:], throughput_two, throughput_avg_two)
 
-    # latencies_one = get_latencies(dir_one)
-    # latencies_two = get_latencies(dir_two)
-    # plot_latencies(out_dir, latencies_one, latencies_two)
+    # Plot memory in time, memory ccdf, unmatched items in time, unmatched items ccdf, and latency ccdf
+
+    dir_one = "server_load_45000_accel_0_time_7200_real_lats"
+    dir_two = "server_load_45000_accel_0_time_7200_dummy_lats2"
+
+    items_one = parse_unmatched(dir_one)
+    ts_one, total_unmatched_one, throughput_one, throughput_avg_one = process_unmatched(items_one)
+
+    mems_one = parse_memories(dir_one)
+    mems_two = parse_memories(dir_two)
+    plot_memory_ccdfs(out_dir, mems_one, mems_two)
+    plot_memory_in_time(out_dir, mems_one, mems_two)
+    plot_unmatched_in_time(out_dir, ts_one, total_unmatched_one)
+    plot_unmatched_ccdf(out_dir, total_unmatched_one)
+    # plot_unmatched_histogram(out_dir, total_unmatched_one)
+
+    latencies = get_latencies(dir_one)
+    plot_latencies(out_dir, latencies)
 
 
 if __name__ == '__main__':
